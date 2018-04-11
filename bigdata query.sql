86fe6c6abad55aca3eb4c30fa4fa7948dd95e63f
Aron Einhorn <aron2560@gmail.com>
2018-04-11 14:29:21 -0400

    Fixed unique query

--- bigdata query.sql
+++ bigdata query.sql
@@ -1,18 +1,18 @@
 SELECT
-  f.repo_name,
-  f.path,
+  MIN(f.repo_name),
+  MIN(f.path),
   c.pbkey
 FROM
   [bigquery-public-data:github_repos.files] f
 JOIN (
   SELECT
-    MIN(id) id,
+    id,
     REGEXP_EXTRACT(content, r'(?:^|[^a-zA-Z0-9])(5[HJK][1-9a-km-zA-HJ-NP-Z]{48,49})(?:$|[^a-zA-Z0-9])') pbkey
   FROM
     [bigquery-public-data:github_repos.contents]
   WHERE
-    REGEXP_MATCH(content, r'(?:^|[^a-zA-Z0-9])(5[HJK][1-9a-km-zA-HJ-NP-Z]{48,49})(?:$|[^a-zA-Z0-9])')
-  GROUP BY
-    pbkey) c
+    REGEXP_MATCH(content, r'(?:^|[^a-zA-Z0-9])(5[HJK][1-9a-km-zA-HJ-NP-Z]{48,49})(?:$|[^a-zA-Z0-9])')) c
 ON
   f.id = c.id
+GROUP BY
+  c.pbkey
