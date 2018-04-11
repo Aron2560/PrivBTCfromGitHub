c50b0cee5d01967310b7a36a5b0ab6806dfacc0f
Aron Einhorn <aron2560@gmail.com>
2018-04-11 14:09:08 -0400

    Fixed to get uniqe values

--- bigdata query.sql
+++ bigdata query.sql
@@ -3,14 +3,16 @@ SELECT
   f.path,
   c.pbkey
 FROM
-  [bigquery-public-data:github_repos.files]  f
+  [bigquery-public-data:github_repos.files] f
 JOIN (
   SELECT
-    id,
+    MIN(id) id,
     REGEXP_EXTRACT(content, r'(?:^|[^a-zA-Z0-9])(5[HJK][1-9a-km-zA-HJ-NP-Z]{48,49})(?:$|[^a-zA-Z0-9])') pbkey
   FROM
-    [bigquery-public-data:github_repos.contents] 
+    [bigquery-public-data:github_repos.contents]
   WHERE
-    REGEXP_MATCH(content, r'(?:^|[^a-zA-Z0-9])(5[HJK][1-9a-km-zA-HJ-NP-Z]{48,49})(?:$|[^a-zA-Z0-9])') ) c
+    REGEXP_MATCH(content, r'(?:^|[^a-zA-Z0-9])(5[HJK][1-9a-km-zA-HJ-NP-Z]{48,49})(?:$|[^a-zA-Z0-9])')
+  GROUP BY
+    pbkey) c
 ON
   f.id = c.id
